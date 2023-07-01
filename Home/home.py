1a8d9162962ffd430a21df051d699e5842c1c00f
wolfrox71 <wolfrox71@gmail.com>
2023-07-01 18:37:10 +0100

    Added a paths file to dynamically create paths in index

--- Home/home.py
+++ Home/home.py
@@ -1,7 +1,20 @@
 from flask import Blueprint, render_template
-
+from os.path import join, dirname, realpath
 home_bp = Blueprint("home_bp", __name__, template_folder="templates/")
+
+def getPaths(filename="paths.txt"):
+    filepath = join(dirname(realpath(__file__)), filename) # join the path to this file + the path to the text file with the values in
+    with open(filepath, "r") as f:
+        lines = f.readlines()
+    paths = {}
+    
+    for line in lines:
+        path, name = line.split(":")
+        paths[path] = name.strip()
+    return paths
+
 
 @home_bp.route("/")
 def index():
-    return render_template("index.html")
+    print(getPaths())
+    return render_template("index.html", paths=getPaths())
